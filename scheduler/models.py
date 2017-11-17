4b762ca5ca427cc2b3d403554e7e0a3631b54526
Onur R. Bingol <orbingol@gmail.com>
2017-11-16 20:04:25 -0600

    Update models according to DB schema v7

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,15 +1,6 @@
 from django.db import models
 
 
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    capacity = models.IntegerField()
-
-    class Meta:
-        verbose_name = 'Course'
-        verbose_name_plural = 'Courses'
-
-
 class Building(models.Model):
     name = models.CharField(max_length=100)
     coord_x = models.FloatField(default=0.0)
@@ -20,27 +11,10 @@ class Building(models.Model):
         verbose_name_plural = 'Buildings'
 
 
-class Exam(models.Model):
-    EXAM_TYPES = (
-        ('Midterm', 'MIDTERM'),
-        ('Final', 'FINAL')
-    )
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    type = models.CharField(max_length=10, choices=EXAM_TYPES, default='Midterm')
-    building = models.ForeignKey(Building, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'Exam'
-        verbose_name_plural = 'Exams'
-
-
 class Classroom(models.Model):
     name = models.CharField(max_length=100, default='Classroom')
     capacity = models.IntegerField()
-    max_ta = models.PositiveIntegerField()
-    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    max_ta = models.PositiveIntegerField(verbose_name='Max Assignable TAs')
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     class Meta:
@@ -49,20 +23,45 @@ class Classroom(models.Model):
 
 
 class ClassroomSchedule(models.Model):
-    timeslot = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'Classroom Busy TimeSlot'
-        verbose_name_plural = 'Classroom Busy TimeSlots'
+        verbose_name = 'Classroom Schedule'
+        verbose_name_plural = 'Classroom Schedules'
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=100, verbose_name='Course Code')
+    capacity = models.IntegerField()
+    building = models.ForeignKey(Building, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+
+class Exam(models.Model):
+    EXAM_TYPES = (
+        ('Midterm', 'MIDTERM'),
+        ('Final', 'FINAL')
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    type = models.CharField(max_length=10, choices=EXAM_TYPES, default='Midterm')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Exam'
+        verbose_name_plural = 'Exams'
 
 
 class TA(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=None, verbose_name='Course if head TA')
     building = models.ForeignKey(Building, on_delete=models.CASCADE)
 
     class Meta:
@@ -71,9 +70,20 @@ class TA(models.Model):
 
 
 class TASchedule(models.Model):
-    timeslot = models.DateTimeField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = 'TA Busy TimeSlot'
-        verbose_name_plural = 'TA Busy TimeSlots'
+        verbose_name = 'TA Schedule'
+        verbose_name_plural = 'TA Schedules'
+
+
+class TAExam(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    ta = models.ForeignKey(TA, on_delete=models.CASCADE, verbose_name='TA')
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Exam - TA Schedule'
+        verbose_name_plural = 'Exam - TA Schedules'
