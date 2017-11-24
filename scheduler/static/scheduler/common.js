5a47f25e55cd2350809dac28de0cca7d3241874d
Onur R. Bingol <orbingol@gmail.com>
2017-11-23 23:30:06 -0600

    Update templates

---
+++ scheduler/static/scheduler/common.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    // $("form input[name='classroom_schedule_min_time']").click(function () {
+    //     $("form input[name='classroom_schedule_min_time']").datepicker();
+    // });
+    // $("form input[name='classroom_schedule_max_time']").click(function () {
+    //     $("form input[name='classroom_schedule_max_time']").datepicker();
+    // });
+    // $("form input[name='ta_schedule_min_time']").click(function () {
+    //     $("form input[name='ta_schedule_min_time']").datepicker();
+    // });
+    // $("form input[name='ta_schedule_max_time']").click(function () {
+    //     $("form input[name='ta_schedule_max_time']").datepicker();
+    // });
+});
