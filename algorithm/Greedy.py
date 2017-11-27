a03409a9f99e976c92a67688e1828b8e7b46787c
Onur R. Bingol <orbingol@gmail.com>
2017-11-27 13:30:42 -0600

    Fix typos in Rahul's example

--- algorithm/Greedy.py
+++ algorithm/Greedy.py
@@ -2,11 +2,12 @@ from scheduler import models
 
 from datetime import datetime, date, time, timedelta
 
+
 def test_function(midterm_start, midterm_end, final_start, final_end):
     # midterm_* and final_* vars are date objects
     # example: midterm_start is an date object
     ta_list = models.TA.objects.all()
-    for ta in ta_list
+    for ta in ta_list:
         # ta is your TA class in models.py
         # ta.first_name, ta.last_time, etc.
         pass
@@ -18,7 +19,6 @@ def test_function(midterm_start, midterm_end, final_start, final_end):
         for sched in schedule:
             # sched.start_time as a datetime object
             pass
-
 
     # Save to DB
     dbq = models.TAExam()
